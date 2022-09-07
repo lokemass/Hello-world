9250f55add9834276045ef5ca8f1a458e081b478
lokeshwar <60348595+lokemass@users.noreply.github.com>
2022-09-07 13:55:05 +0530

    initial commit

---
+++ src/com/sample/Sub.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Sub {
+    public static void main(String[] args) {
+
+        System.out.println("Enter two numbers");
+        int first = 30;
+        int second = 20;
+
+        System.out.println(first + " " + second);
+
+        // add two numbers
+        int sub = first - second;
+        System.out.println("The Sub is: " + sub);
+    }
+}
